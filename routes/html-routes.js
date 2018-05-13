cf8523d0b74183a78fd15673a95001b306f62d37
SigmaVX <tonywible@gmail.com>
2018-05-12 22:07:44 -0400

    added updates for new routes and hbs

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -55,9 +55,9 @@ module.exports = function(app) {
         db.Users.findAll({}).
         then(function (uData) {
           hbsObject.users = uData;
-          console.log("open and pending topics: " + JSON.stringify(hbsObject.openAndPending));
-          console.log("closed topics: " + JSON.stringify(hbsObject.closed));
-          console.log("users data: " + JSON.stringify(hbsObject.users));
+          console.log("Open And Pending Topics: " + JSON.stringify(hbsObject.openAndPending));
+          console.log("Closed Topics: " + JSON.stringify(hbsObject.closed));
+          console.log("usersData: " + JSON.stringify(hbsObject.users));
 
           res.render("topics", hbsObject);
         });
