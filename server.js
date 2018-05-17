8ee95ea9bfdc1cdf0a6571d7da4798a31c297053
Fabian Flores <ffflores1@outlook.com>
2018-05-17 16:41:11 -0400

    authentication feature file structure

--- server.js
+++ server.js
@@ -2,7 +2,12 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var session = require("express-session");
+
 require("dotenv").config();
+
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 
 // Express App & Parser Setup
 // =============================================================
@@ -11,6 +16,16 @@ var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static("public"));
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({
+                  secret: "keyboard cat",
+                  resave: true,
+                  saveUninitialized: true 
+                }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Setup Handlebars View Engine
 // =============================================================
@@ -43,6 +58,6 @@ require("./routes/choices-api-routes.js")(app);
 // =============================================================
 db.sequelize.sync({ force: false }).then(function() {
     app.listen(PORT, function() {
-        console.log("App listening on PORT " + PORT);
+        console.log("==> 🌎   App listening on PORT %s, http://localhost:%s ", PORT, PORT);
     });
 });
